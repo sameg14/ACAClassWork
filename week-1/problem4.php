609dbbc5d686c7c15766f6cf1d609f18266f8f13
air <sameg14@gmail.com>
2015-02-05 21:07:04 -0600

    Added cards shell

---
+++ week-1/problem4.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Return an array that represents a deck of cards
+ *
+ * e.g. array(
+ *  0 => '1D', // Ace of diamonds
+ *  1 => '2D', // 2 of diamonds
+ *  ...
+ *  11 => '11C' // Jack of clubs
+ * )
+ *
+ * @return array
+ */
+function getDeck()
+{
+
+}
+
+/**
+ * Shuffle the deck of cards
+ *
+ * @param array $deck Full deck of cards (passed by ref)
+ *
+ * @return void
+ */
+function shuffleDeck(&$deck)
+{
+
+}
+
+/**
+ * @param array $players      An array of player names
+ * @param int   $numCards     How many cards to give each player
+ * @param array $shuffledDeck A shuffled deck of cards
+ *
+ * @return array
+ */
+function deal($players, $numCards, &$shuffledDeck)
+{
+
+}
+
+
+// ----------- USAGE -----------------
+
+// Crack open a brand new deck of cards
+$deck = getDeck();
+
+// Shuffle the deck
+shuffleDeck($deck);
+
+echo 'Deck after shuffling, but before dealing: <br/>';
+print_r($deck);
+
+$players = array('Joe', 'Mary', 'Zim');
+$numCards = 3;
+
+$playerHands = deal($players, $numCards, $deck);
+
+echo 'Hands each player has: <br/>';
+print_r($playerHands);
+
+echo 'Deck after dealing: <br/>';
+print_r($deck);
+
+
+echo '<span style="color:red;">&diams;</span>';
+echo PHP_EOL;
+echo '&clubs;';
+echo PHP_EOL;
+echo '<span style="color:red;">&hearts;</span>';
+echo PHP_EOL;
+echo '&spades;';
